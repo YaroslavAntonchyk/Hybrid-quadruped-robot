14bafab05d266a6b7cdbe7dc8f6a92603ab89454
Yaroslav Antonchyk <yaroslav.antonchik@gmail.com>
2020-05-09 16:53:26 +0200

    Initial commit

---
+++ Debug/StdPeriph_Driver/src/stm32f10x_gpio.d
@@ -0,0 +1,95 @@
+StdPeriph_Driver/src/stm32f10x_gpio.o: \
+ ../StdPeriph_Driver/src/stm32f10x_gpio.c \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ F:/STM_projects/Yasha/CMSIS/device/stm32f10x.h \
+ F:/STM_projects/Yasha/CMSIS/core/core_cm3.h \
+ F:/STM_projects/Yasha/CMSIS/core/core_cmInstr.h \
+ F:/STM_projects/Yasha/CMSIS/core/core_cmFunc.h \
+ F:/STM_projects/Yasha/CMSIS/device/system_stm32f10x.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_conf.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_adc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_bkp.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_can.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_cec.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_crc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dac.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dma.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_exti.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_flash.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_fsmc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_gpio.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_i2c.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_iwdg.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_pwr.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rcc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rtc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_sdio.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_spi.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_tim.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_usart.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_wwdg.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/misc.h \
+ F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rcc.h
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_gpio.h:
+
+F:/STM_projects/Yasha/CMSIS/device/stm32f10x.h:
+
+F:/STM_projects/Yasha/CMSIS/core/core_cm3.h:
+
+F:/STM_projects/Yasha/CMSIS/core/core_cmInstr.h:
+
+F:/STM_projects/Yasha/CMSIS/core/core_cmFunc.h:
+
+F:/STM_projects/Yasha/CMSIS/device/system_stm32f10x.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_conf.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_adc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_bkp.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_can.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_cec.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_crc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dac.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dbgmcu.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_dma.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_exti.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_flash.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_fsmc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_gpio.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_i2c.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_iwdg.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_pwr.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rcc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rtc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_sdio.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_spi.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_tim.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_usart.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_wwdg.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/misc.h:
+
+F:/STM_projects/Yasha/StdPeriph_Driver/inc/stm32f10x_rcc.h:
---
+++ src/main.c
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f10x.h"
+#include "stm32f1xx_nucleo.h"
+			
+
+int main(void)
+{
+	int i = 1;
+
+	while(1)
+	{
+		i++;
+	}
+}
